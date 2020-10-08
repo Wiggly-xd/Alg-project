ed3abd4a84f0158e8f5c1d139d995f158a72d4b9
moch1772 <ziggs1772@gmail.com>
2020-10-08 09:19:44 +0200

    added shapefile to shapefold Property of shapefile

--- testKARL.py
+++ testKARL.py
@@ -3,18 +3,43 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import shapefile
 import matplotlib
+import matplotlib.patches as patches
 import os
 #from scipy import imageio
 import numpy
+def drawRedSquare(image, location, colour):
+
+    x,y = location
+    w,h = 58.789155,66.210835
+
+    for row in range(h):
+        for col in range(w):
+            image[row+y][col+x] = colour
+
+    return image
+
 file=shapefile.Reader("img_shp/Export_Output_387.shp");
-#file=shapefile.Reader("countries.shp")
-rec=file.record(2)
+#file=shapefile.Reader("img_shp/AFO6.shp");
+#rec=file.record(2)
 fig=plt.figure(figsize=(10, 10))
 ax=fig.gca()
 shapes=file.shapes()
-print(len(shapes))
+howManyShapes=len(shapes)
+s=file.shape()
+shape=shapes[0].points
+contain=file.bbox
+print("ShapePoints",shape)
+print("Kordinater",contain)
 
-for i in range(len(shapes)):
+längd=contain[2]-contain[0]
+höjd=contain[3]-contain[1]
+print("Längd= ",längd)
+print("Höjd= ",höjd)
+print("Propottioner= ",höjd/längd)
+print(file)
+#Kordinat av shape 0
+print("Shape coord",len(shapes[0].bbox))
+for i in range(howManyShapes):
     try:
         poly=file.shape(i).__geo_interface__
         ax.add_patch(PolygonPatch(poly, fc='#ffffff', ec='#000000', alpha=0.5, zorder=2 ))
@@ -22,18 +47,18 @@ for i in range(len(shapes)):
         ax.append(fig.add_subplot(5, 4))
         fig(num=1, figsize=(8, 6))
         ax.patch.set_alpha(0.5)
-
     except:
         print (i)
 img=Image.open(r"img_shp/1406_S1_2020.bmp")
+ax.imshow(img)
         #plt.imshow(img)
 
-#img=Image.open("img.png")
 print("true")
 pixVal=list(img.getdata())
 #x, y = (img > 1000).nonzero()
 #vals=img[x, y];
 width, height=0,0
+cordinates=width,height
 Mwidth, Mheight = img.size
 outside=0
 Skogsmark=0
@@ -68,10 +93,7 @@ print("avverkad ",avverkad)
 print("total size ",size)
 print("Inte med ",size-outside-Skogsmark-Vatten-Myrmark-houses-avverkad)
 
-    #if(i!=(0, 255, 0) and i!=(204, 232, 182) and i!=(252, 251, 210) and i!=(242, 213, 199)and i!=(224, 160, 128) and i!=(197, 224, 245)):
+plt.figure()
+plt.imshow(img)
 
-#img=img.resize((6000, 8000))
-#plt.subplot(1, 2, 1)
-plt.imshow(img, alpha=0.5)
-
-#plt.show()
+plt.show()
