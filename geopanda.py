1667c8b2d7d309d52a77fba2bc502cb18d074b83
nibarsito <nibar@hotmail.se>
2020-10-27 11:15:22 +0100

    nu går det att hita koordinater i shape filen

--- geopanda.py
+++ geopanda.py
@@ -8,8 +8,15 @@ gdf1 = gpd.read_file('img_shp/Export_Output_387.shp')
 
 gdf = gpd.GeoDataFrame(pd.concat([gdf1]))
 frame=gdf.bounds
-print(frame.iloc[3])
-
+print(frame.iloc[1])
+# create a point object containing the coordinates of the poit you want to check. this is needed later. then in this example we call the function then take a polygon via the index from our gdf data frame. and then extract the shape from that index.  
+p1=Point(419633, 6484949)
+def isWithin(point, frame):
+    for i in range(62):
+        if point.within(frame.iloc[i].geometry):
+            print ("true")
+            print(i)
+isWithin(p1, gdf)
 #print(frame.iloc[1, 1])
 
 gdf.head()
